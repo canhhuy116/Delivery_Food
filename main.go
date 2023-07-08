c67929bb8e347f5d1f6ea5fdd4d3ee9f64df2908
canhhuy116 <canhhuy1162002@gmail.com>
2023-07-08 20:59:50 +0700

    Initialize

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	//refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := os.Getenv("DBConnectionStr")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := runService(db); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func runService(db *gorm.DB) error {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	return r.Run()
+}
